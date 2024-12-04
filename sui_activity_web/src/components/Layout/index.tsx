6c70c2a4b1ea2a82e84a41e25458a8a1b0833c8a
ZCDC_Ren <renzw@zzes1314.cn>
2024-12-04 11:47:58 +0800

    fix: 优化注册后刷新状态

--- sui_activity_web/src/components/Layout/index.tsx
+++ sui_activity_web/src/components/Layout/index.tsx
@@ -1,64 +1,22 @@
-import { Avatar, Button, Layout, Modal } from 'antd'
+import { Layout } from 'antd'
 import router from '../../routers/router'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { ConnectButton, useCurrentAccount, useSuiClientQuery } from '@mysten/dapp-kit'
 import '@mysten/dapp-kit/dist/index.css'
 import './style.less'
-import RegisterForm from '../RegisterForm'
+import MemberInfo from '../MemberInfo'
 import { useNetworkVariable } from '/@/utils/networkConfig'
-import { LoadingOutlined } from '@ant-design/icons'
 import CreateActivityBtn from '../CreateActivityBtn'
 import { JoinActivityData, useUserStore } from '../../stores/user'
-
-interface MemberData {
-  avatar: string
-  description: string
-  id: {
-    id: string
-  }
-  index: string
-  name: string
-  nickname: string
-  sex: string
-  url: string
-}
 
 const { Header, Content } = Layout
 
 export default function PageLayout() {
   const packageId = useNetworkVariable('packageId')
-  const [isRegisterModalOpen, setIsRegisterModalOpen] = useState(false)
-  const [userData, setUserData] = useState<MemberData>()
-  const registerFormRef = useRef<{ submitForm: () => void; resetForm: () => void }>(null)
-  const needRefetch = useRef(false)
   const account = useCurrentAccount()
-  const { setUser, setJoinActivityList, setActivityListRefetch } = useUserStore()
-  // 查找会员卡
-  const { data: memberQueryData, isPending, refetch } = useSuiClientQuery(
-    'getOwnedObjects',
-    {
-      owner: account?.address || '',
-      limit: 1,
-      filter: {
-        MatchAll: [
-          {
-            StructType: `${packageId}::member::MemberNft`,
-          },
-          {
-            AddressOwner: account?.address!!,
-          },
-        ],
-      },
-      options: {
-        showDisplay: true,
-        showContent: true,
-      },
-    },
-    {
-      gcTime: 10000,
-    },
-  )
+  const { setJoinActivityList, setActivityListRefetch } = useUserStore()
+ 
   // 查找参与的活动
   const { data: joinQueryData, refetch: joinRefetch } = useSuiClientQuery(
     'getOwnedObjects',
@@ -85,58 +43,11 @@ export default function PageLayout() {
   )
 
   /**
-   * 打开注册会员弹窗
-   */
-  const openRegisterMember = () => {
-    setIsRegisterModalOpen(true)
-  }
-  /**
-   * 注册会员表单提交
-   */
-  const handleRegisterMember = () => {
-    registerFormRef.current?.submitForm()
-  }
-  /**
-   * 注册会员成功
-   */
-  const registerSuccess = async () => {
-    needRefetch.current = true
-    const result = await refetch()
-    console.log('refetch', result)
-    if (result.data && result.data.data.length > 0 && result.data.data[0].data?.content?.dataType === 'moveObject') {
-      setUserData(result.data.data[0].data.content.fields as unknown as MemberData)
-    } else {
-      setUserData(undefined)
-      registerSuccess()
-    }
-    setIsRegisterModalOpen(false)
-  }
-  /**
    * 初始化时设置活动列表刷新函数
    */
   useEffect(() => {
     setActivityListRefetch(joinRefetch)
   }, [])
-  /**
-   * 监听会员信息
-   */
-  useEffect(() => {
-    let memeberData: MemberData | undefined = undefined
-    if (memberQueryData && memberQueryData.data.length > 0 && memberQueryData.data[0].data?.content?.dataType === 'moveObject') {
-      memeberData = memberQueryData.data[0].data.content.fields as unknown as MemberData
-      setUser({
-        objectId: memberQueryData.data[0].data.objectId,
-        name: memeberData.name,
-        nickname: memeberData.nickname,
-        description: memeberData.description,
-        sex: memeberData.sex,
-        avatar: memeberData.avatar,
-      })
-      setUserData(memeberData)
-    } else {
-      setUserData(undefined)
-    }
-  }, [memberQueryData])
 
   /**
    * 监听参与活动列表
@@ -163,19 +74,7 @@ export default function PageLayout() {
           <div className="page-title ml-5 text-white font-bold text-xl">SuiHi 随嗨</div>
         </div>
         <div className="flex items-center justify-between">
-          {
-            isPending ?
-            <LoadingOutlined /> :
-            userData ?
-            <div className="flex items-center">
-              <Avatar
-                size="default"
-                icon={<img src={userData.avatar as string} />}
-              />
-              <div className="flex items-center ml-2 mr-5">欢迎回家，<div className="font-bold text-green-500">{userData.nickname as string}</div></div>
-            </div> :
-            <Button className="mr-5" type="primary" onClick={openRegisterMember}>申请会员</Button>
-          }
+          <MemberInfo />
           <ConnectButton className="reset-connect-button" connectText="使用钱包登录"></ConnectButton>
         </div>
       </Header>
@@ -186,9 +85,6 @@ export default function PageLayout() {
           </React.StrictMode>
         </div>
       </Content>
-      <Modal title="申请会员" open={isRegisterModalOpen} onOk={handleRegisterMember} onCancel={() => setIsRegisterModalOpen(false)}>
-        <RegisterForm ref={registerFormRef} onSuccess={() => registerSuccess}></RegisterForm>
-      </Modal>
       <CreateActivityBtn />
     </Layout>
   )
---
+++ sui_activity_web/src/components/MemberInfo.tsx
@@ -0,0 +1,204 @@
+import { PlusOutlined } from '@ant-design/icons'
+import { Avatar, Button, Form, Input, Modal, Radio, Spin, Upload } from 'antd'
+import ImgCrop from 'antd-img-crop'
+import TextArea from 'antd/es/input/TextArea'
+import { UploadImageApi } from '../apis/common.api'
+import { forwardRef, useEffect, useState } from 'react'
+import { Transaction } from '@mysten/sui/transactions'
+import { useNetworkVariable } from '../utils/networkConfig'
+import { SUI_HAI_SERVER, WALRUS_AGGREGATOR } from '../utils/constants'
+import { useCurrentAccount, useSignAndExecuteTransaction, useSuiClientQuery } from '@mysten/dapp-kit'
+import { message } from 'antd'
+import { useUserStore } from '../stores/user'
+
+interface MemberData {
+  avatar: string
+  description: string
+  id: {
+    id: string
+  }
+  index: string
+  name: string
+  nickname: string
+  sex: string
+  url: string
+}
+
+const RegisterForm = forwardRef(() => {
+  const [messageApi, contextHolder] = message.useMessage()
+  const [form] = Form.useForm()
+  const packageId = useNetworkVariable('packageId')
+  const [isRegisterModalOpen, setIsRegisterModalOpen] = useState(false)
+  const [userData, setUserData] = useState<MemberData>()
+  const account = useCurrentAccount()
+  const { mutate } = useSignAndExecuteTransaction()
+  const { setUser } = useUserStore()
+
+  // 查找会员卡
+  const { data: memberQueryData, isPending, refetch: refetchMemberInfo } = useSuiClientQuery(
+    'getOwnedObjects',
+    {
+      owner: account?.address || '',
+      limit: 1,
+      filter: {
+        MatchAll: [
+          {
+            StructType: `${packageId}::member::MemberNft`,
+          },
+          {
+            AddressOwner: account?.address!!,
+          },
+        ],
+      },
+      options: {
+        showDisplay: true,
+        showContent: true,
+      },
+    },
+    {
+      gcTime: 10000,
+    },
+  )
+  
+  /**
+   * 自定义上传逻辑
+   */
+  const customUpload = async (options: any) => {
+    const { file, onSuccess, onError, onProgress } = options
+    const res = await UploadImageApi(file)
+    if (res) {
+      onProgress(100)
+      let blobId = ''
+      if (res.alreadyCertified) {
+        blobId = res.alreadyCertified.blobId
+      } else if (res.newlyCreated) {
+        blobId = res.newlyCreated.blobObject.blobId
+      }
+      onSuccess(blobId)
+      form.setFieldValue('avatar', blobId)
+    } else {
+      onError('上传失败')
+    }
+  }
+  const onFinish = (values: any) => {
+    console.log(values)
+    const txb = new Transaction()
+    txb.moveCall({
+      target: `${packageId}::sui_hai::add_memeber`,
+      arguments: [
+        txb.object(SUI_HAI_SERVER),
+        txb.pure.string(values.name),
+        txb.pure.string(values.description),
+        txb.pure.string(values.sex),
+        txb.pure.string(WALRUS_AGGREGATOR[0] +values.avatar),
+      ]
+    })
+    mutate(
+      {
+        transaction: txb
+      },
+      {
+        onError: (err) => {
+          console.log(err.message)
+          messageApi.error(err.message)
+        },
+        onSuccess: (result) => {
+          form.resetFields()
+          registerSuccessHandler()
+          messageApi.success(`注册成功: ${result.digest}`)
+        },
+      }
+    )
+  }
+
+  /**
+   * 注册会员成功
+   */
+  const registerSuccessHandler = async () => {
+    setIsRegisterModalOpen(false)
+    console.log('refetchMemberInfo')
+    const result = await refetchMemberInfo()
+    console.log('refetch', result)
+    if (result.data && result.data.data.length > 0 && result.data.data[0].data?.content?.dataType === 'moveObject') {
+      setUserData(result.data.data[0].data.content.fields as unknown as MemberData)
+    } else {
+      setUserData(undefined)
+    }
+  }
+
+    /**
+   * 监听会员信息
+   */
+  useEffect(() => {
+    let memeberData: MemberData | undefined = undefined
+    if (memberQueryData && memberQueryData.data.length > 0 && memberQueryData.data[0].data?.content?.dataType === 'moveObject') {
+      memeberData = memberQueryData.data[0].data.content.fields as unknown as MemberData
+      setUser({
+        objectId: memberQueryData.data[0].data.objectId,
+        name: memeberData.name,
+        nickname: memeberData.nickname,
+        description: memeberData.description,
+        sex: memeberData.sex,
+        avatar: memeberData.avatar,
+      })
+      setUserData(memeberData)
+    } else {
+      setUserData(undefined)
+    }
+  }, [memberQueryData])
+  return (
+    <>
+      {contextHolder}
+      {
+        isPending ?
+        <Spin /> :
+        userData ?
+        <div className="flex items-center">
+          <Avatar
+            size="default"
+            icon={<img src={userData.avatar as string} />}
+          />
+          <div className="flex items-center ml-2 mr-5">欢迎回家，<div className="font-bold text-green-500">{userData.nickname as string}</div></div>
+        </div> :
+        <Button className="mr-5" type="primary" onClick={() =>setIsRegisterModalOpen(true)}>申请会员</Button>
+      }
+      <Modal title="申请会员" open={isRegisterModalOpen} onOk={() => form.submit()} onCancel={() => setIsRegisterModalOpen(false)}>
+        <Form
+          form={form}
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 14 }}
+          layout="horizontal"
+          style={{ maxWidth: 600 }}
+          variant="filled"
+          onFinish={onFinish}
+        >
+          <Form.Item label="昵称：" name="name" rules={[{ required: true, message: '请输入昵称!' }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="性别：" name="sex" rules={[{ required: true, message: '请选择性别!' }]}>
+            <Radio.Group>
+              <Radio value="未知"> 未知 </Radio>
+              <Radio value="女"> 女 </Radio>
+              <Radio value="男"> 男 </Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item label="描述：" name="description" rules={[{ required: true, message: '请输入描述!' }]}>
+            <TextArea rows={4} />
+          </Form.Item>
+          <Form.Item label="头像：" name="avatar" rules={[{ required: true, message: '请上传头像!' }]}>
+            <ImgCrop rotationSlider modalOk="确定" modalCancel="取消">
+              <Upload customRequest={customUpload} multiple={false} maxCount={1} listType="picture-card">
+                <button style={{ border: 0, background: 'none' }} type="button">
+                  <PlusOutlined />
+                  <div style={{ marginTop: 8 }}>Upload</div>
+                </button>
+              </Upload>
+            </ImgCrop>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  )
+})
+
+export default RegisterForm
